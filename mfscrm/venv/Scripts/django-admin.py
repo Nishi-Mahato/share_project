d65c5d90d29ddbdcab15c94784218dfd726f8332
amiguhaamin <career.rashmikguhaamin@gmail.com>
2019-11-17 14:44:55 -0600

    Python Djando maverick food service project.

---
+++ mfscrm/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\02_Study\1_MS\1_MIS_at_UNO\ISQA4900-Internet_System_Development\Assignment2\mfscrm\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mfscrm/venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!D:\02_Study\1_MS\1_MIS_at_UNO\ISQA4900-Internet_System_Development\Assignment2\mfscrm\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
